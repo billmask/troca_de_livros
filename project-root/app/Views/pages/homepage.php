e4f68aa96cc7bec10a6fb501fb3afc9d0e6dc5e7
Carlos Alberto Silva Júnior <carlosalbertospace@hotmail.com>
2022-10-29 17:51:39 -0300

    Change: frase na home page

--- project-root/app/Views/pages/homepage.php
+++ project-root/app/Views/pages/homepage.php
@@ -2,7 +2,7 @@
     <div class="col-12 m-3">
         <h1 class="text-center">Doe os livros que você não usa mais</h1>
         <div class="separador m-3"></div>
-        <p class="text-center">Ajude estudantes que não tem condições que comprar livros para um melhor aprendizado.</p>
+        <p class="text-center">Ajude estudantes que não tem condições de comprar livros para um melhor aprendizado.</p>
         <p class="d-flex justify-content-center"><a href="<?= base_url('auth/registration'); ?>" class="nav-link btn btn-primary text-white col-4">Registrar como doador</a></p>
     </div>
     <div class="col-12 m-3">
