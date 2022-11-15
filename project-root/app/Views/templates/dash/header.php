6b66ad7cefca04122faefc958d05ee475e38e2d7
Carlos Alberto Silva Júnior <carlosalbertospace@hotmail.com>
2022-11-15 20:05:53 -0300

    Add: link de listagem de livros no dash

--- project-root/app/Views/templates/dash/header.php
+++ project-root/app/Views/templates/dash/header.php
@@ -7,10 +7,13 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link text-white" aria-current="page" href="<?= base_url('dashboard/livros'); ?>">Livros</a>
+          <a class="nav-link text-white" aria-current="page" href="<?= base_url('dashboard/livros'); ?>">Meus livros</a>
         </li>
         <li class="nav-item">
           <a class="nav-link text-white" href="<?= base_url('dashboard/contatos'); ?>">Contatos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="<?= base_url('livros'); ?>">Todos os livros</a>
         </li>
       </ul>
       <ul class="navbar-nav">
