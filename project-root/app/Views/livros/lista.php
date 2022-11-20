1803f207eac60533576a61f3f88497ea6606dc80
Carlos Alberto Silva Júnior <carlosalbertospace@hotmail.com>
2022-11-20 16:59:09 -0300

    Fix: link principal

--- project-root/app/Views/livros/lista.php
+++ project-root/app/Views/livros/lista.php
@@ -10,49 +10,51 @@
         <div class='alert alert-success'><?= session()->getFlashdata('success'); ?></div>
     <?php endif ?>
     <!-- Fim de mensagens de erro -->
-    <ul class="list-group lista-livros">
-        <?php
-        if (!$livros) {
-            echo '<p>Nenhum livro disponível para doação.</p>';
-        } else {
-        ?>
-            <?php if (isset($_GET['buscar'])) echo "<h3>Pesquisa para <strong>" . $_GET['buscar'] . "</strong></h3>"; ?>
-            <h1>Livros</h1>
-            <?php foreach ($livros as $livro) { ?>
-                <li class="list-group-item list-group-item-action flex-column align-items-start p-3 item-animal">
-                    <div class="d-flex">
-                        <div class="img-animal col-2 d-flex  justify-content-center">
-                            <img src="<?= $livro['url_img']; ?>" alt="Livro <?= $livro['nome']; ?>" class="img-fluid" style="border-radius: 10px; max-height: 150px;">
-                        </div>
-                        <div class="dados-animal col-10 d-flex align-items-center">
-                            <div class="conteudo col-12 p-3">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <h2 class="mb-1">
-                                        <caption><?= $livro['nome']; ?> - <?= $livro['edicao']; ?>° edição</caption>
-                                    </h2>
-                                    <small class="badge bg-dark text-white"><?= $livro['ano']; ?></small>
-                                </div>
-                                <div class="d-flex w-100 justify-content-between pt-2">
-                                    <div class="texto">
-                                        <p class="mb-1"><strong>Autor:</strong> <?= $livro['autor']; ?></p>
-                                        <p class="mb-1"><strong>Editora:</strong> <?= $livro['editora']; ?></p>
-                                        <p class="mb-1"><strong>Doador:</strong> <?= $livro['nome_usuario']; ?></p>
-                                        <p class="mb-1"><strong>Local para buscar livro: </strong><?= $livro['cidade']; ?>-<?= $livro['estado']; ?></p>
+    <h1><a href="http://#livros"></a> Livros</h1>
+    <main id="livros">
+        <ul class="list-group lista-livros">
+            <?php
+            if (!$livros) {
+                echo '<p>Nenhum livro disponível para doação.</p>';
+            } else {
+            ?>
+                <?php if (isset($_GET['buscar'])) echo "<h3>Pesquisa para <strong>" . $_GET['buscar'] . "</strong></h3>"; ?>
+                <?php foreach ($livros as $livro) { ?>
+                    <li class="list-group-item list-group-item-action flex-column align-items-start p-3 item-animal">
+                        <div class="d-flex">
+                            <div class="img-animal col-2 d-flex  justify-content-center">
+                                <img src="<?= $livro['url_img']; ?>" alt="Livro <?= $livro['nome']; ?>" class="img-fluid" style="border-radius: 10px; max-height: 150px;">
+                            </div>
+                            <div class="dados-animal col-10 d-flex align-items-center">
+                                <div class="conteudo col-12 p-3">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h2 class="mb-1">
+                                            <caption><?= $livro['nome']; ?> - <?= $livro['edicao']; ?>° edição</caption>
+                                        </h2>
+                                        <small class="badge bg-dark text-white"><?= $livro['ano']; ?></small>
                                     </div>
-                                    <div class="botao d-flex align-items-end flex-column">
-                                        <p><strong><?= $livro['tema']; ?></strong></p>
-                                        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#livro-<?= $livro['id']; ?>">
-                                            Entrar em contato
-                                        </button>
+                                    <div class="d-flex w-100 justify-content-between pt-2">
+                                        <div class="texto">
+                                            <p class="mb-1"><strong>Autor:</strong> <?= $livro['autor']; ?></p>
+                                            <p class="mb-1"><strong>Editora:</strong> <?= $livro['editora']; ?></p>
+                                            <p class="mb-1"><strong>Doador:</strong> <?= $livro['nome_usuario']; ?></p>
+                                            <p class="mb-1"><strong>Local para buscar livro: </strong><?= $livro['cidade']; ?>-<?= $livro['estado']; ?></p>
+                                        </div>
+                                        <div class="botao d-flex align-items-end flex-column">
+                                            <p><strong><?= $livro['tema']; ?></strong></p>
+                                            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#livro-<?= $livro['id']; ?>">
+                                                Entrar em contato
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-        <?php }
-        } ?>
-    </ul>
+                    </li>
+            <?php }
+            } ?>
+        </ul>
+    </main>
 </section>
 
 <!-- Conteudo visível relevante vai dentro da main -->
